fb1254902e0e795edfb491609db718fa79a30cc8
aldmbmtl <alex.e.hatfield@gmail.com>
2021-01-24 19:49:31 -0500

    POC for luna

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import logo from './luna.png';
+import {Navbar, Row, Col} from "react-bootstrap";
+import hubble from './hubble.png';
+import luna from './luna.png';
 import './App.css';
 import Luna from './luna/luna';
 
 function App() {
 
-  return (
-    <header className="App-header">
-        <Luna/>
-        <img src={logo} alt="" width="40%"/>
-    </header>
-  );
+    return (
+        <>
+            <Navbar>
+                <Navbar.Brand>
+                    <img src={hubble} alt="" width="5%"/>
+                </Navbar.Brand>
+            </Navbar>
+            <header className="App-header">
+                <Luna/>
+                <img src={luna} alt="" width="40%"/>
+            </header>
+        </>
+    );
 }
 
 export default App;
